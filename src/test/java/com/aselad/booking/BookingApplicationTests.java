eb7f75379f10f17437e05b4adcf533d2a881b93f
Asela Dasanayaka <aselarbd@gmail.com>
2022-08-13 19:46:40 +0530

    initial commit

---
+++ src/test/java/com/aselad/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aselad.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
